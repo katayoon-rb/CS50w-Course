9f07f271ac2aef003fe041c2a23ac8d99ffacdda
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-09-13 22:05:49 +0330

    Add files via upload

---
+++ prj3 Commerce/auctions/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("create", views.create_listing, name="create"),
+    path("display_cat", views.display_cat, name="display_cat"),
+    path("listing/<int:id>", views.listing, name="listing"),
+    path("remove_watchlist/<int:id>", views.remove_watchlist, name="remove_watchlist"),
+    path("add_watchlist/<int:id>", views.add_watchlist, name="add_watchlist"),
+    path("watchlist", views.watchlist, name="watchlist"),
+    path("add_comment/<int:id>", views.add_comment, name="add_comment"),
+    path("add_bid/<int:id>", views.add_bid, name="add_bid"),
+    path("close_auction/<int:id>", views.close_auction, name="close_auction"),
+]
