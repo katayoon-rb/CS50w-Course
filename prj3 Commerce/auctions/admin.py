e86a113464d164dc6d3e4f272cc123c35eae9e53
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-11-05 12:45:23 +0330

    Delete prj3 Commerce directory

--- prj3 Commerce/auctions/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(User)
-admin.site.register(Category)
-admin.site.register(Listing)
-admin.site.register(Comment)
-admin.site.register(Bid)
